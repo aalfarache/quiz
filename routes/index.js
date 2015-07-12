4ba70a9914489058c0294185a46557fe907bb3ec
aalfarache <aal4004@gmail.com>
2015-07-13 00:15:49 +0200

    Página de créditos/autor.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+var authorController = require('../controllers/author_controller');
 var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
@@ -11,5 +12,8 @@ router.get('/', function(req, res) {
 /* GET quizes page. */
 router.get('/quizes/question', quizController.question);
 router.get('/quizes/answer', quizController.answer);
+
+/* GET creditos page. */
+router.get('/author', authorController.author);
 
 module.exports = router;
