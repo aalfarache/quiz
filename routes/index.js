88de67f262bc9bedf4771f31e845c742dea5f222
aalfarache <aal4004@gmail.com>
2015-08-02 21:56:41 +0200

    Revert "Mod 8 -"
    This reverts commit d6cb8274e7028c2f1679cb3d7f23f0d6e3b1eac1.

--- routes/index.js
+++ routes/index.js
@@ -19,7 +19,6 @@ router.get('/quizes/new',                   quizController.new);
 router.post('/quizes/create',               quizController.create);
 router.get('/quizes/:quizId(\\d+)/edit',    quizController.edit);
 router.put('/quizes/:quizId(\\d+)',         quizController.update);
-router.delete('/quizes/:quizId(\\d+)',      quizController.destroy);
 
 /* GET creditos page. */
 router.get('/author', authorController.author);
